bef252dddd980a5b1477e280c34b18ac4d76d0a4
yuyang <yh5525@nyu.edu>
2024-10-30 09:39:48 -0400

    test

---
+++ cmake-build-debug/CMakeFiles/meanreversiontradingstrategy.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/meanreversiontradingstrategy.dir/dataCalculations.cpp.o"
+  "CMakeFiles/meanreversiontradingstrategy.dir/dataCalculations.cpp.o.d"
+  "CMakeFiles/meanreversiontradingstrategy.dir/main.cpp.o"
+  "CMakeFiles/meanreversiontradingstrategy.dir/main.cpp.o.d"
+  "CMakeFiles/meanreversiontradingstrategy.dir/tradeSession.cpp.o"
+  "CMakeFiles/meanreversiontradingstrategy.dir/tradeSession.cpp.o.d"
+  "meanreversiontradingstrategy"
+  "meanreversiontradingstrategy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/meanreversiontradingstrategy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
